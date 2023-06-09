c4239a5fa13411e49bbaaf9c0cc31b2a3c692db8
marcosmanfre <marcosmanfre@hotmail.com>
2023-06-08 21:06:37 -0300

    views

--- backend/register/views.py
+++ backend/register/views.py
@@ -10,33 +10,6 @@ from django.core.files.storage import default_storage
 
 
 @csrf_exempt
-def employeeApi(request,id=0):
-    if request.method=='GET':
-        employees = Employees.objects.all()
-        employees_serializer=EmployeeSerializer(employees,many=True)
-        return JsonResponse(employees_serializer.data,safe=False)
-    elif request.method=='POST':
-        employee_data=JSONParser().parse(request)
-        employees_serializer=EmployeeSerializer(data=employee_data)
-        if employees_serializer.is_valid():
-            employees_serializer.save()
-            return JsonResponse("Added Successfully",safe=False)
-        return JsonResponse("Failed to Add",safe=False)
-    elif request.method=='PUT':
-        employee_data=JSONParser().parse(request)
-        employee=Employees.objects.get(EmployeeId=employee_data['EmployeeId'])
-        employees_serializer=EmployeeSerializer(employee,data=employee_data)
-        if employees_serializer.is_valid():
-            employees_serializer.save()
-            return JsonResponse("Updated Successfully",safe=False)
-        return JsonResponse("Failed to Update")
-    elif request.method=='DELETE':
-        employee=Employees.objects.get(EmployeeId=id)
-        employee.delete()
-        return JsonResponse("Deleted Successfully",safe=False)
-
-
-@csrf_exempt
 def pessoaApi(request, id=0):
     if request.method == 'GET':
         pessoa = Pessoa.objects.all()
