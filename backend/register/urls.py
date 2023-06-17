62cca48c4f4bd7659ff8659abf2ac3d1ef671494
marcosmanfre <marcosmanfre@hotmail.com>
2023-06-17 19:55:18 -0300

    urls

--- backend/register/urls.py
+++ backend/register/urls.py
@@ -7,7 +7,6 @@ from django.conf import settings
 urlpatterns=[
     url(r'^employee$',views.employeeApi),
     url(r'^employee/([0-9]+)$',views.employeeApi),
-    url(r'^employee$',views.employeeApi),
-    url(r'^employee/([0-9]+)$',views.employeeApi),
+
     
 ]
